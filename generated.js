388e378de84486a5d653e74df8a4a4e5e7b8eb94
Arjun Phull <arjunphull@arizona.edu>
2023-02-21 01:36:00 -0700

    adding all files

---
+++ generated.js
@@ -0,0 +1,603 @@
+// load adams county
+const adams = new THREE.Group()
+adams.position.set(0.9869, 0.25, 3.4059)
+scene.add(adams)
+loader.load('adams.gltf', function(gltf) {
+  adams.add(gltf.scene)
+})
+
+
+// load allegheny county
+const allegheny = new THREE.Group()
+allegheny.position.set(-5.6384, 0.25, 1.684)
+scene.add(allegheny)
+loader.load('allegheny.gltf', function(gltf) {
+  allegheny.add(gltf.scene)
+})
+
+
+// load armstrong county
+const armstrong = new THREE.Group()
+armstrong.position.set(-4.2803, 0.25, 0.4335)
+scene.add(armstrong)
+loader.load('armstrong.gltf', function(gltf) {
+  armstrong.add(gltf.scene)
+})
+
+
+// load beaver county
+const beaver = new THREE.Group()
+beaver.position.set(-6.3648, 0.25, 0.9463)
+scene.add(beaver)
+loader.load('beaver.gltf', function(gltf) {
+  beaver.add(gltf.scene)
+})
+
+
+// load bedford county
+const bedford = new THREE.Group()
+bedford.position.set(-2.0057, 0.25, 3.0102)
+scene.add(bedford)
+loader.load('bedford.gltf', function(gltf) {
+  bedford.add(gltf.scene)
+})
+
+
+// load berks county
+const berks = new THREE.Group()
+berks.position.set(3.8702, 0.25, 1.7836)
+scene.add(berks)
+loader.load('berks.gltf', function(gltf) {
+  berks.add(gltf.scene)
+})
+
+
+// load blair county
+const blair = new THREE.Group()
+blair.position.set(-1.7488, 0.25, 1.564)
+scene.add(blair)
+loader.load('blair.gltf', function(gltf) {
+  blair.add(gltf.scene)
+})
+
+
+// load bradford county
+const bradford = new THREE.Group()
+bradford.position.set(2.5393, 0.25, -2.4107)
+scene.add(bradford)
+loader.load('bradford.gltf', function(gltf) {
+  bradford.add(gltf.scene)
+})
+
+
+// load bucks county
+const bucks = new THREE.Group()
+bucks.position.set(5.9648, 0.25, 1.966)
+scene.add(bucks)
+loader.load('bucks.gltf', function(gltf) {
+  bucks.add(gltf.scene)
+})
+
+
+// load butler county
+const butler = new THREE.Group()
+butler.position.set(-5.3726, 0.25, 0.2032)
+scene.add(butler)
+loader.load('butler.gltf', function(gltf) {
+  butler.add(gltf.scene)
+})
+
+
+// load cambria county
+const cambria = new THREE.Group()
+cambria.position.set(-2.5378, 0.25, 1.5838)
+scene.add(cambria)
+loader.load('cambria.gltf', function(gltf) {
+  cambria.add(gltf.scene)
+})
+
+
+// load cameron county
+const cameron = new THREE.Group()
+cameron.position.set(-1.3469, 0.25, -1.2855)
+scene.add(cameron)
+loader.load('cameron.gltf', function(gltf) {
+  cameron.add(gltf.scene)
+})
+
+
+// load carbon county
+const carbon = new THREE.Group()
+carbon.position.set(4.4243, 0.25, 0.1466)
+scene.add(carbon)
+loader.load('carbon.gltf', function(gltf) {
+  carbon.add(gltf.scene)
+})
+
+
+// load centre county
+const centre = new THREE.Group()
+centre.position.set(-0.3158, 0.25, 0.0911)
+scene.add(centre)
+loader.load('centre.gltf', function(gltf) {
+  centre.add(gltf.scene)
+})
+
+
+// load chester county
+const chester = new THREE.Group()
+chester.position.set(4.4621, 0.25, 3.0851)
+scene.add(chester)
+loader.load('chester.gltf', function(gltf) {
+  chester.add(gltf.scene)
+})
+
+
+// load clarion county
+const clarion = new THREE.Group()
+clarion.position.set(-4.2577, 0.25, -0.648)
+scene.add(clarion)
+loader.load('clarion.gltf', function(gltf) {
+  clarion.add(gltf.scene)
+})
+
+
+// load clearfield county
+const clearfield = new THREE.Group()
+clearfield.position.set(-1.8659, 0.25, 0.0369)
+scene.add(clearfield)
+loader.load('clearfield.gltf', function(gltf) {
+  clearfield.add(gltf.scene)
+})
+
+
+// load clinton county
+const clinton = new THREE.Group()
+clinton.position.set(0.0221, 0.25, -0.6748)
+scene.add(clinton)
+loader.load('clinton.gltf', function(gltf) {
+  clinton.add(gltf.scene)
+})
+
+
+// load columbia county
+const columbia = new THREE.Group()
+columbia.position.set(2.8023, 0.25, -0.1574)
+scene.add(columbia)
+loader.load('columbia.gltf', function(gltf) {
+  columbia.add(gltf.scene)
+})
+
+
+// load crawford county
+const crawford = new THREE.Group()
+crawford.position.set(-5.6531, 0.25, -2.1255)
+scene.add(crawford)
+loader.load('crawford.gltf', function(gltf) {
+  crawford.add(gltf.scene)
+})
+
+
+// load cumberland county
+const cumberland = new THREE.Group()
+cumberland.position.set(0.9188, 0.25, 2.6602)
+scene.add(cumberland)
+loader.load('cumberland.gltf', function(gltf) {
+  cumberland.add(gltf.scene)
+})
+
+
+// load dauphin county
+const dauphin = new THREE.Group()
+dauphin.position.set(1.9904, 0.25, 1.8685)
+scene.add(dauphin)
+loader.load('dauphin.gltf', function(gltf) {
+  dauphin.add(gltf.scene)
+})
+
+
+// load delaware county
+const delaware = new THREE.Group()
+delaware.position.set(5.2815, 0.25, 3.2071)
+scene.add(delaware)
+loader.load('delaware.gltf', function(gltf) {
+  delaware.add(gltf.scene)
+})
+
+
+// load elk county
+const elk = new THREE.Group()
+elk.position.set(-2.423, 0.25, -1.2859)
+scene.add(elk)
+loader.load('elk.gltf', function(gltf) {
+  elk.add(gltf.scene)
+})
+
+
+// load erie county
+const erie = new THREE.Group()
+erie.position.set(-5.6292, 0.25, -3.3038)
+scene.add(erie)
+loader.load('erie.gltf', function(gltf) {
+  erie.add(gltf.scene)
+})
+
+
+// load fayette county
+const fayette = new THREE.Group()
+fayette.position.set(-4.7987, 0.25, 3.263)
+scene.add(fayette)
+loader.load('fayette.gltf', function(gltf) {
+  fayette.add(gltf.scene)
+})
+
+
+// load forest county
+const forest = new THREE.Group()
+forest.position.set(-3.746, 0.25, -1.4879)
+scene.add(forest)
+loader.load('forest.gltf', function(gltf) {
+  forest.add(gltf.scene)
+})
+
+
+// load franklin county
+const franklin = new THREE.Group()
+franklin.position.set(-0.3511, 0.25, 3.0713)
+scene.add(franklin)
+loader.load('franklin.gltf', function(gltf) {
+  franklin.add(gltf.scene)
+})
+
+
+// load fulton county
+const fulton = new THREE.Group()
+fulton.position.set(-1.1734, 0.25, 3.2589)
+scene.add(fulton)
+loader.load('fulton.gltf', function(gltf) {
+  fulton.add(gltf.scene)
+})
+
+
+// load greene county
+const greene = new THREE.Group()
+greene.position.set(-6.1437, 0.25, 3.4159)
+scene.add(greene)
+loader.load('greene.gltf', function(gltf) {
+  greene.add(gltf.scene)
+})
+
+
+// load huntingdon county
+const huntingdon = new THREE.Group()
+huntingdon.position.set(-0.8006, 0.25, 1.8438)
+scene.add(huntingdon)
+loader.load('huntingdon.gltf', function(gltf) {
+  huntingdon.add(gltf.scene)
+})
+
+
+// load indiana county
+const indiana = new THREE.Group()
+indiana.position.set(-3.5283, 0.25, 1.0965)
+scene.add(indiana)
+loader.load('indiana.gltf', function(gltf) {
+  indiana.add(gltf.scene)
+})
+
+
+// load jefferson county
+const jefferson = new THREE.Group()
+jefferson.position.set(-3.1238, 0.25, -0.4424)
+scene.add(jefferson)
+loader.load('jefferson.gltf', function(gltf) {
+  jefferson.add(gltf.scene)
+})
+
+
+// load juniata county
+const juniata = new THREE.Group()
+juniata.position.set(0.6663, 0.25, 1.6058)
+scene.add(juniata)
+loader.load('juniata.gltf', function(gltf) {
+  juniata.add(gltf.scene)
+})
+
+
+// load lackawanna county
+const lackawanna = new THREE.Group()
+lackawanna.position.set(4.616, 0.25, -1.3076)
+scene.add(lackawanna)
+loader.load('lackawanna.gltf', function(gltf) {
+  lackawanna.add(gltf.scene)
+})
+
+
+// load lancaster county
+const lancaster = new THREE.Group()
+lancaster.position.set(3.1465, 0.25, 2.9948)
+scene.add(lancaster)
+loader.load('lancaster.gltf', function(gltf) {
+  lancaster.add(gltf.scene)
+})
+
+
+// load lawrence county
+const lawrence = new THREE.Group()
+lawrence.position.set(-6.2702, 0.25, -0.0493)
+scene.add(lawrence)
+loader.load('lawrence.gltf', function(gltf) {
+  lawrence.add(gltf.scene)
+})
+
+
+// load lebanon county
+const lebanon = new THREE.Group()
+lebanon.position.set(2.8593, 0.25, 1.9085)
+scene.add(lebanon)
+loader.load('lebanon.gltf', function(gltf) {
+  lebanon.add(gltf.scene)
+})
+
+
+// load lehigh county
+const lehigh = new THREE.Group()
+lehigh.position.set(4.7354, 0.25, 1.1595)
+scene.add(lehigh)
+loader.load('lehigh.gltf', function(gltf) {
+  lehigh.add(gltf.scene)
+})
+
+
+// load luzerne county
+const luzerne = new THREE.Group()
+luzerne.position.set(3.8818, 0.25, -0.5519)
+scene.add(luzerne)
+loader.load('luzerne.gltf', function(gltf) {
+  luzerne.add(gltf.scene)
+})
+
+
+// load lycoming county
+const lycoming = new THREE.Group()
+lycoming.position.set(1.3982, 0.25, -1.0289)
+scene.add(lycoming)
+loader.load('lycoming.gltf', function(gltf) {
+  lycoming.add(gltf.scene)
+})
+
+
+// load mckean county
+const mckean = new THREE.Group()
+mckean.position.set(-2.2178, 0.25, -2.4706)
+scene.add(mckean)
+loader.load('mckean.gltf', function(gltf) {
+  mckean.add(gltf.scene)
+})
+
+
+// load mercer county
+const mercer = new THREE.Group()
+mercer.position.set(-6.1273, 0.25, -0.9364)
+scene.add(mercer)
+loader.load('mercer.gltf', function(gltf) {
+  mercer.add(gltf.scene)
+})
+
+
+// load mifflin county
+const mifflin = new THREE.Group()
+mifflin.position.set(0.0757, 0.25, 1.2215)
+scene.add(mifflin)
+loader.load('mifflin.gltf', function(gltf) {
+  mifflin.add(gltf.scene)
+})
+
+
+// load monroe county
+const monroe = new THREE.Group()
+monroe.position.set(5.4087, 0.25, -0.1849)
+scene.add(monroe)
+loader.load('monroe.gltf', function(gltf) {
+  monroe.add(gltf.scene)
+})
+
+
+// load montgomery county
+const montgomery = new THREE.Group()
+montgomery.position.set(5.375, 0.25, 2.356)
+scene.add(montgomery)
+loader.load('montgomery.gltf', function(gltf) {
+  montgomery.add(gltf.scene)
+})
+
+
+// load montour county
+const montour = new THREE.Group()
+montour.position.set(2.2726, 0.25, -0.0949)
+scene.add(montour)
+loader.load('montour.gltf', function(gltf) {
+  montour.add(gltf.scene)
+})
+
+
+// load northampton county
+const northampton = new THREE.Group()
+northampton.position.set(5.3738, 0.25, 0.6818)
+scene.add(northampton)
+loader.load('northampton.gltf', function(gltf) {
+  northampton.add(gltf.scene)
+})
+
+
+// load northumberland county
+const northumberland = new THREE.Group()
+northumberland.position.set(2.2568, 0.25, 0.3333)
+scene.add(northumberland)
+loader.load('northumberland.gltf', function(gltf) {
+  northumberland.add(gltf.scene)
+})
+
+
+// load perry county
+const perry = new THREE.Group()
+perry.position.set(0.7894, 0.25, 1.8412)
+scene.add(perry)
+loader.load('perry.gltf', function(gltf) {
+  perry.add(gltf.scene)
+})
+
+
+// load philadelphia county
+const philadelphia = new THREE.Group()
+philadelphia.position.set(5.9485, 0.25, 2.9794)
+scene.add(philadelphia)
+loader.load('philadelphia.gltf', function(gltf) {
+  philadelphia.add(gltf.scene)
+})
+
+
+// load pike county
+const pike = new THREE.Group()
+pike.position.set(6.05, 0.25, -1.1532)
+scene.add(pike)
+loader.load('pike.gltf', function(gltf) {
+  pike.add(gltf.scene)
+})
+
+
+// load potter county
+const potter = new THREE.Group()
+potter.position.set(-0.645, 0.25, -2.2683)
+scene.add(potter)
+loader.load('potter.gltf', function(gltf) {
+  potter.add(gltf.scene)
+})
+
+
+// load schuylkill county
+const schuylkill = new THREE.Group()
+schuylkill.position.set(3.2829, 0.25, 0.8268)
+scene.add(schuylkill)
+loader.load('schuylkill.gltf', function(gltf) {
+  schuylkill.add(gltf.scene)
+})
+
+
+// load snyder county
+const snyder = new THREE.Group()
+snyder.position.set(1.2844, 0.25, 0.725)
+scene.add(snyder)
+loader.load('snyder.gltf', function(gltf) {
+  snyder.add(gltf.scene)
+})
+
+
+// load somerset county
+const somerset = new THREE.Group()
+somerset.position.set(-3.3397, 0.25, 3.0601)
+scene.add(somerset)
+loader.load('somerset.gltf', function(gltf) {
+  somerset.add(gltf.scene)
+})
+
+
+// load sullivan county
+const sullivan = new THREE.Group()
+sullivan.position.set(2.5625, 0.25, -1.3508)
+scene.add(sullivan)
+loader.load('sullivan.gltf', function(gltf) {
+  sullivan.add(gltf.scene)
+})
+
+
+// load susquehanna county
+const susquehanna = new THREE.Group()
+susquehanna.position.set(4.1974, 0.25, -2.5845)
+scene.add(susquehanna)
+loader.load('susquehanna.gltf', function(gltf) {
+  susquehanna.add(gltf.scene)
+})
+
+
+// load tioga county
+const tioga = new THREE.Group()
+tioga.position.set(0.8811, 0.25, -2.3809)
+scene.add(tioga)
+loader.load('tioga.gltf', function(gltf) {
+  tioga.add(gltf.scene)
+})
+
+
+// load union county
+const union = new THREE.Group()
+union.position.set(1.2556, 0.25, 0.0662)
+scene.add(union)
+loader.load('union.gltf', function(gltf) {
+  union.add(gltf.scene)
+})
+
+
+// load venango county
+const venango = new THREE.Group()
+venango.position.set(-4.9182, 0.25, -1.2646)
+scene.add(venango)
+loader.load('venango.gltf', function(gltf) {
+  venango.add(gltf.scene)
+})
+
+
+// load warren county
+const warren = new THREE.Group()
+warren.position.set(-3.7975, 0.25, -2.5239)
+scene.add(warren)
+loader.load('warren.gltf', function(gltf) {
+  warren.add(gltf.scene)
+})
+
+
+// load washington county
+const washington = new THREE.Group()
+washington.position.set(-6.0466, 0.25, 2.3287)
+scene.add(washington)
+loader.load('washington.gltf', function(gltf) {
+  washington.add(gltf.scene)
+})
+
+
+// load wayne county
+const wayne = new THREE.Group()
+wayne.position.set(5.4447, 0.25, -1.9706)
+scene.add(wayne)
+loader.load('wayne.gltf', function(gltf) {
+  wayne.add(gltf.scene)
+})
+
+
+// load westmoreland county
+const westmoreland = new THREE.Group()
+westmoreland.position.set(-4.2793, 0.25, 1.9439)
+scene.add(westmoreland)
+loader.load('westmoreland.gltf', function(gltf) {
+  westmoreland.add(gltf.scene)
+})
+
+
+// load wyoming county
+const wyoming = new THREE.Group()
+wyoming.position.set(3.7603, 0.25, -1.6192)
+scene.add(wyoming)
+loader.load('wyoming.gltf', function(gltf) {
+  wyoming.add(gltf.scene)
+})
+
+
+// load york county
+const york = new THREE.Group()
+york.position.set(2.2353, 0.25, 3.1607)
+scene.add(york)
+loader.load('york.gltf', function(gltf) {
+  york.add(gltf.scene)
+})
+
+
